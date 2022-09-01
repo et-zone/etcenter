a2b62a97a67de7b9a30f5eaef0aa8d3f2903b0d1
guanzhouyi <zhouyi.guan@carsome.com>
2022-09-01 17:18:17 +0800

    test

--- test_main.go
+++ test_main.go
@@ -2,16 +2,15 @@ package etcenter
 
 import (
 	"context"
-	redis "github.com/go-redis/redis/v8"
 	"testing"
 )
 
 func testMain(t testing.T){
-	cli:= NewClient(redis.NewClient(&redis.Options{
+	cli:= NewClient(Options{
 		Addr:     "49.232.190.11:63790",
 		Password: "", // no password set
 		DB:       1,  // use default DB
-	}))
+	})
 	w,_:= NewWorker(context.TODO(),cli,"vvvv5vvvv",3,10)
 	w.SyncWatch(func() {}, func() {})
 }
